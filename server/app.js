05a6e316c01ecef1b9ae4f79130e4f62327581ce
Ted Karanikolas <ted@hrg-nyc.com>
2016-07-21 10:58:49 -0500

    more backend

--- server/app.js
+++ server/app.js
@@ -6,19 +6,22 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.use(express.static('public'));
+
+//static folder
+app.use(express.static('public'));
 
 //will need to declare databse below
 var pg = require('pg');
 var connectionString = '**';
 
+//base url
+app.get( '/', function (req, res){
+  res.sendFile( path.resolve( 'views/index.html') );
+});
+
+//spin up server
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
     console.log('Server is up and listening on', app.get('port'));
 });
-
-
-app.get('/', function(req, res){
-  res.sendFile(path.resolve('views/index.html'));
-});
-
-app.use(express.static('public'));

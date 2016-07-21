5b6a225ca6bcfeacb9713913ce6bf68fa33478b8
Ted Karanikolas <ted@hrg-nyc.com>
2016-07-21 10:50:35 -0500

    server routes/modules added

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var app=express();
+var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 

08ffa28bb3226c21c8026b92cc96b254cb0c570e
Ted Karanikolas <ted@hrg-nyc.com>
2016-07-23 11:26:53 -0500

    nothing

--- server/app.js
+++ server/app.js
@@ -1,23 +1,21 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-
 var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
 
-app.use(express.static('public'));
+app.use(bodyParser.json());
 
 //static folder
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
-//will need to declare databse below
+//will need to declare database below
 var pg = require('pg');
 var connectionString = '**';
 
 //base url
 app.get( '/', function (req, res){
-  res.sendFile( path.resolve( 'views/index.html') );
+  res.sendFile( path.resolve('views/index.html') );
 });
 
 //spin up server

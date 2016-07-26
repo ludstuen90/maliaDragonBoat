79867ad73a2dad394202f268620c03d9ef54dfb1
Craig <craigjwaldron@gmail.com>
2016-07-26 08:59:52 -0500

    styling & home

--- public/scripts/controllers/adminController.js
+++ public/scripts/controllers/adminController.js
@@ -88,4 +88,26 @@ console.log('in adminController');
      }; // end hotelRequest function
 
 
-     }]); // end adminController
+// TAB FUNCTIONALITY ----------------------------------
+     $scope.tabs = [{
+             title: 'One',
+             url: 'one.tpl.html'
+         }, {
+             title: 'Two',
+             url: 'two.tpl.html'
+         }, {
+             title: 'Three',
+             url: 'three.tpl.html'
+     }];
+
+     $scope.currentTab = 'one.tpl.html';
+
+     $scope.onClickTab = function (tab) {
+         $scope.currentTab = tab.url;
+     };
+
+     $scope.isActiveTab = function(tabUrl) {
+         return tabUrl == $scope.currentTab;
+     };
+
+}]); // end adminController

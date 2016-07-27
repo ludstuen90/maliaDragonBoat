cfa37aee123dc70c2641230890357ad256729b02
Susan Rehl <susanrehl@gmail.com>
2016-07-27 16:47:27 -0500

    merge

--- public/scripts/clientApp.js
+++ public/scripts/clientApp.js
@@ -1,4 +1,10 @@
-var DRGNBT = angular.module('DRGNBT', ['ngRoute']);
+var DRGNBT = angular.module('DRGNBT', ['ngRoute', 'xeditable']);
+// , ['xeditable']
+
+//CODE FOR XEDITABLE --------------------------------------
+DRGNBT.run(function(editableOptions) {
+  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
 
 // Configure routes
 
--- public/scripts/controllers/adminController.js
+++ public/scripts/controllers/adminController.js
@@ -1,4 +1,4 @@
-DRGNBT.controller('adminController', ['$scope', '$http', '$window', function($scope, $http, $window){
+DRGNBT.controller('adminController', ['$scope', '$http', '$window', '$filter', function($scope, $http, $window, $filter){
 console.log('in adminController');
     var objectToSend={}; // creates global object to send
 
@@ -71,12 +71,9 @@ console.log('in adminController');
              });
            });
 
-    $scope.eventAndSurveyRequest = function() {  //runs both eventRequest and surveyRequest queries to display event and survey results on adminSurvey.html
-      eventRequest();
-      surveyRequest();
-    };
 
-    var eventList=[];
+
+    $scope.eventList=[];
 
      $scope.eventRequest = function() { // gets info for current event to display on for Admin survey page
        console.log("in eventRequest function in adminController");
@@ -111,6 +108,11 @@ console.log('in adminController');
        }// end error function
        ); // end then response
      }; // end surveyRequest function
+
+     $scope.eventAndSurveyRequest = function() {  //runs both eventRequest and surveyRequest queries to display event and survey results on adminSurvey.html
+       eventRequest();
+       surveyRequest();
+     };
 
      var hotelList=[];
 
@@ -187,5 +189,70 @@ console.log('in adminController');
      $scope.showEvent = function(){
        console.log(sessionStorage.getItem("eventId"));
      };
+
+// ROOM BUILDER FUNCTIONALITY -----------------------------
+
+$scope.outsideArray=[];
+
+$scope.getRoom = function() {
+  console.log("in getRoom function in script");
+  roomToSend = {
+       room_number: $scope.roomnum
+   };
+   console.log(roomToSend);
+  $http({   // gets recordset via POST
+    method: 'POST',
+    url: '/getRoom',
+    data: roomToSend
+  }).then(function() {
+    $scope.showRoom();
+  });
+}; // end getRoom function
+
+$scope.roomToShow = [];
+
+$scope.showRoom = function() {
+console.log("in show room function in script");
+$http({   // gets recordset via GET
+  method: 'GET',
+  url: '/showRoom',
+}).then( function(response){  // success call - runs function with response parameter
+// console.log(response.data);
+  $scope.roomToShow = response.data;  // pulls the data from app.js and sets to global var roomToShow
+  console.log($scope.roomToShow);
+}, function myError(response){
+  console.log(response.statusText);
+}// end error function
+); // end then response
+$scope.roomnum="";
+}; // end showRoom function
+
+$scope.makeSqlHappy=function(recordroom_number, recordroom_type, recordcapacity, recordprice, recordcheck_in_date, recordcheck_out_date, recordnotes, recordid) {
+console.log('in makeSqlHappy');
+console.log("data from makeSqlHappy: ", recordroom_number, recordroom_type, recordcapacity, recordprice, recordcheck_in_date, recordcheck_out_date, recordnotes);
+console.log("makeSqlHappy's stinkin id: ", recordid);
+var id=recordid;
+var data={
+  room_number: recordroom_number,
+  room_type: recordroom_type,
+  capacity: recordcapacity,
+  price: recordprice,
+  check_in_date: recordcheck_in_date,
+  check_out_date: recordcheck_out_date,
+  notes: recordnotes
+};
+console.log(data);
+$http({
+  method: 'PUT',
+  url: '/saveRoom/' +id,
+  data: data
+});
+};
+// END XEDITABLE ROOM ASSIGNER CODE -----------------
+
+
+
+
+
 
 }]); // end adminController

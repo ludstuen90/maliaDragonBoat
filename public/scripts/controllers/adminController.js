214c0fc4ff8b9a1b8e9a0980d7346a3357757280
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-08-01 13:27:35 -0500

    admin edits

--- public/scripts/controllers/adminController.js
+++ public/scripts/controllers/adminController.js
@@ -3,12 +3,28 @@ DRGNBT.controller('adminController', ['$scope', '$http', '$window', '$filter', f
     var objectToSend={}; // creates global object to send
 
 
+
     $scope.assignEvent = function(eventId){
         console.log(eventId);
         sessionStorage.setItem("eventId", eventId);
       $scope.mango = sessionStorage.getItem("eventId");
       console.log("mango is: ", $scope.mango);
-      // console.log("We have saved mango as ", $scope.mango);
+
+
+        $scope.tabChange = function($scope) {
+          $scope.selectedTab = 0;
+
+         	console.log($scope.selectedTab);
+           $scope.changeNavigation = function() {
+             console.log('Hello World1');
+             $scope.selectedTab = 2;
+           };
+         };
+
+         $scope.tabChange(1);
+
+
+               // console.log("We have saved mango as ", $scope.mango);
     };
 
 
@@ -369,16 +385,22 @@ $http({
 
 $scope.pageLoad = function(){
 
-$scope.hotelRequest();
 
 
-$scope.eventRequest();
-
-$scope.getEvents = function(){
-  console.log('made it to getEvents!');
+$scope.fetchEvents = function(){
+  $http({
+    method: 'GET',
+    url: '/eventPopulate'
+  }).then( function( response ) {
+    $scope.alltheEvents = response.data;
+    console.log('and the response data is: ');
+    console.log( response.data );
+  });
 };
 
-$scope.getEvents();
+$scope.hotelRequest();
+$scope.eventRequest();
+$scope.fetchEvents();
 
 };
 
